99f097fd2ab1d008c751d4c1abe62f8468de2396
ines <thisisinesbabe@gmail.com>
2026-09-16 20:02:29 +0100

    infinit messages till the client disconnects from the server

--- client.py
+++ client.py
@@ -2,8 +2,8 @@ import socket
 
 PORT = 5050
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECTED"
-SERVER = "192.168.1.36"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.1.36" #here you should use the computer private ipv4 that u are using for the server
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,8 +14,15 @@ client.connect(ADDR)
 def send_message(msg):
     message = msg.encode(FORMAT)
     client.send(message)
-    print(client.recv(2048).decode(FORMAT))
+    print(f"[{SERVER}]:{client.recv(2048).decode(FORMAT)}")
 
-send_message(input("You:"))
 
+while True:
+    message = input("You: ")
+    send_message(message)
+
+    if message == DISCONNECT_MESSAGE:
+        break
+
+    
 
--- server.py
+++ server.py
@@ -5,7 +5,7 @@ PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECTED"
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
@@ -28,12 +28,13 @@ def handle_client(conn,addr):
     while connected:
         msg = conn.recv(2048).decode(FORMAT)
         if msg == DISCONNECT_MESSAGE: 
-            break
+            connected = False
         if not msg: 
             break
 
         print(f"[{addr}]:{msg}")
         conn.send(input("You:").encode(FORMAT))
+        
 
     conn.close()
         
